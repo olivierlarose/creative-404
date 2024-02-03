dc460301231d79daadc57ba7c1d83aa76dcf8e7c
Olivier Larose <olivierlarose1@gmail.com>
2024-02-03 17:57:35 -0500

    clean up

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,6 +6,50 @@ import { useControls } from 'leva'
 export default function Model() {
     const { viewport } = useThree()
     const { nodes } = useGLTF('/medias/shards.glb')
+    
+    return (
+            <group scale={viewport.width / 2.75} >
+                {
+                    nodes.Scene.children.map( (mesh, i) => {
+                        return <Mesh data={mesh} key={i}/>
+                    })
+                }
+                <Text />
+                <Background />
+            </group>
+    )
+}
+
+function Text() {
+    const font = '/fonts/PPNeueMontreal-Bold.otf'
+    const textOption = {
+        color: "white",
+        anchorX: "center",
+        anchorY: "middle"
+    }
+    return (
+        <group>
+            <Text font={font} position={[0, 0, -.1]} fontSize={0.75} {...textOption}>
+            404
+            </Text>
+            <Text font={font} position={[0, -.275, -.1]} fontSize={0.04} {...textOption}>
+            The link is broken
+            </Text>
+        </group>
+    )
+}
+
+function Background() {
+    return (
+        <mesh position={[0, .05, -1]}>
+            <planeGeometry args={[3, 3, 20, 20]}/>
+            <meshStandardMaterial color="black"/>
+        </mesh>
+    )
+}
+
+function Mesh({data}) {
+
     const materialProps = useControls({
         thickness: { value: 0.14, min: 0, max: 20, step: 0.01 },
         roughness: { value: 0, min: 0, max: 1, step: 0.1 },
@@ -16,32 +60,10 @@ export default function Model() {
     })
 
     return (
-            <group scale={viewport.width / 2.75} >
-                {
-                    nodes.Scene.children.map( (mesh, i) => {
-                        return <Float><mesh {...mesh} key={i} receiveShadow={true} castShadow={true}>
-                            <MeshTransmissionMaterial 
-                                {...materialProps}
-                            />
-                        </mesh></Float>
-                    })
-                }
-                <Text font='/fonts/PPNeueMontreal-Bold.otf' position={[0, 0, -.1]} fontSize={0.75} color="white" anchorX="center" anchorY="middle">
-                404
-                </Text>
-                <Text font='/fonts/PPNeueMontreal-Regular.ttf' position={[0, -.275, -.1]} fontSize={0.04} color="white" anchorX="center" anchorY="middle">
-                The link is broken
-                </Text>
-                <mesh position={[0, .05, -1]}>
-                    <planeGeometry args={[3, 3, 20, 20]}/>
-                    <meshStandardMaterial color="black"/>
-                </mesh>
-            </group>
-    )
-}
-
-function Mesh({node}) {
-    return (
-        <mesh {...node}/>
+        <Float>
+            <mesh {...data} receiveShadow={true} castShadow={true}>
+                <MeshTransmissionMaterial {...materialProps}/>
+            </mesh>
+        </Float>
     )
 }
